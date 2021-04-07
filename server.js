768a67892b054b3d2de438a74be93838bbc5251b
Jorge Souza <jorge@vieirasouza.com.br>
2021-04-07 17:52:39 -0300

    using spread syntax in route and creat a function to short the code

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.listen(port, () => {
 });
 
 const User = require("./models/User");
+const { response } = require("express");
 
 mongoose.connect("mongodb://localhost/userData", {
   useUnifiedTopology: true,
@@ -18,99 +19,64 @@ mongoose.connect("mongodb://localhost/userData", {
   useCreateIndex: true,
 });
 
+// SEND RESPONSE
+function sendResponse(res, err, data) {
+  if (err) {
+    res.json({
+      success: false,
+      messa: err,
+    });
+  } else if (!data) {
+    res.json({
+      success: false,
+      message: "Not found",
+    });
+  } else {
+    res.json({
+      success: true,
+      data: data,
+    });
+  }
+}
+
 // CREATE
 app.post("/users", (req, res) => {
   User.create(
-    {
-      name: req.body.newData.name,
-      email: req.body.newData.email,
-      password: req.body.newData.password,
-    },
+    // ERA da forma comentada
+    // {
+    //   name: req.body.newData.name,
+    //   email: req.body.newData.email,
+    //   password: req.body.newData.password,
+    // },
+    { ...req.body.newData },
     (err, data) => {
-      if (err) {
-        res.json({ success: false, message: err });
-      } else if (!data) {
-        res.json({ sucess: false, message: "Nor Found" });
-      } else {
-        res.json({ succes: true, data: data });
-      }
+      sendResponse(res, err, data);
     }
   );
 });
 
+// READ
 app
   .route("/users/:id")
-  // READ
   .get((req, res) => {
     User.findById(req.params.id, (err, data) => {
-      if (err) {
-        res.json({
-          success: false,
-          message: err,
-        });
-      } else if (!data) {
-        res.json({
-          success: false,
-          message: "Not found",
-        });
-      } else {
-        res.json({
-          success: true,
-          data: data,
-        });
-      }
+      sendResponse(res, err, data);
     });
   })
   // UPDATE
   .put((req, res) => {
     User.findByIdAndUpdate(
       req.params.id,
-      {
-        name: req.body.newData.name,
-        email: req.body.newData.email,
-        password: req.body.newData.password,
-      },
-      {
-        new: true,
-      },
+      { ...req.body.newData },
+      { new: true },
       (err, data) => {
-        if (err) {
-          res.json({
-            success: false,
-            message: err,
-          });
-        } else if (!data) {
-          res.json({
-            success: false,
-            message: "Not Found",
-          });
-        } else {
-          res.json({
-            success: true,
-            data: data,
-          });
-        }
+        sendResponse(res, err, data);
       }
     );
   })
   // DELETE
   .delete((req, res) => {
     User.findByIdAndDelete(req.params.id, (err, data) => {
-      if (err) {
-        res.json({
-          success: false,
-          message: err,
-        });
-      } else if (!data) {
-        res.json({
-          success: false,
-          message: "Not Found",
-        });
-      } else {
-        res.json({
-          success: true,
-          data: data,
-        });
-      }
+      sendResponse(res, err, data);
     });
   });
